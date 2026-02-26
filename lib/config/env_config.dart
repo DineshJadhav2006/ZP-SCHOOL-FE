223266c405c690eeb84c83876cefe85fad675faf
DineshJadhav2006 <dj6008676@gmail.com>
2026-02-26 14:51:28 +0530

    setup for production

--- lib/config/env_config.dart
+++ lib/config/env_config.dart
@@ -1,6 +1,7 @@
 class EnvConfig {
   // static const String apiBaseUrl = 'http://localhost:8803';
-  static const String apiBaseUrl = 'http://172.16.22.45:8803';
+  // static const String apiBaseUrl = 'http://172.16.22.45:8803';
+  static const String apiBaseUrl = 'https://zp-school-mandave-kh.onrender.com';
   static const String appName = 'School Management';
   static const bool debugMode = true;
   static const int connectionTimeout = 30000;
